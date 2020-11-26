25b5dac5aae45f70c925db9f409219ddc758790d
Theo Kanning <theokanning@gmail.com>
2020-11-25 19:20:06 -0600

    Add 2019 day 1

---
+++ 2019/1.py
@@ -0,0 +1,17 @@
+def get_fuel_part_2(mass):
+    if (mass <= 6):
+        return 0
+
+    fuel = mass//3 - 2
+    return fuel + get_fuel_part_2(fuel)
+
+with open('inputs/1-1.txt') as f:
+    masses = list(map(int, f))
+
+# Part 1
+fuels = map(lambda x: x//3 - 2, masses)
+print(f"Part 1 answer: {sum(fuels)}")
+
+# Part 2
+fuels = map(get_fuel_part_2, masses)
+print(f"Part 2 answer: {sum(fuels)}")
